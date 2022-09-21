2ec26d5104ed62fc5c1be8eb533a6188372155be
[Joe Davis] <59972343+jdavis511@users.noreply.github.com>
2022-09-20 19:17:40 -0500

    Created new ASP.NET Core with React solution from template.

---
+++ knowYourWellPWA/knowYourWellPWA/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using knowYourWellPWA.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace knowYourWellPWA.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ knowYourWellPWA/knowYourWellPWA/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace knowYourWellPWA.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
