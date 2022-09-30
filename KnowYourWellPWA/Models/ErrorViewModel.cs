636c61f6ec0b7f515ab0ff3f8011e80242035685
fnaif2021 <89944135+fnaif2021@users.noreply.github.com>
2022-09-30 16:04:05 -0500

    I added the project template
    ASP.NET Core MVC

---
+++ KnowYourWellPWA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KnowYourWellPWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
