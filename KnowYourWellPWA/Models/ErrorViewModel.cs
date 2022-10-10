af6ff1277fbbab30f27e418b58a8df14f55ae25d
[Joe Davis] <59972343+jdavis511@users.noreply.github.com>
2022-10-10 14:54:17 -0500

    Rolled back original main branch to blank state.

--- KnowYourWellPWA/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KnowYourWellPWA.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# know-your-well
-2022-2023 Nebraska Water Center Senior Design Capstone Project
