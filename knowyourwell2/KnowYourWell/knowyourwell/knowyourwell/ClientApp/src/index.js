60a3b751a3a5416e45503311fe7bfc7b476c01da
fnaif2021 <fnaif2@unl.edu>
2022-10-29 16:20:48 -0500

    ..

--- knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/ClientApp/src/index.js
+++ knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/ClientApp/src/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
+﻿import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import { createRoot } from 'react-dom/client';
@@ -10,6 +10,46 @@ import reportWebVitals from './reportWebVitals';
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+///start
+
+const express = require('express');
+const db = require('./config/db')
+const cors = require('cors')
+
+const app = express();
+const PORT = 3002;
+app.use(cors());
+app.use(express.json())
+
+// Route to get all posts
+app.get("/api/get", (req, res) => {
+    db.query("SELECT * FROM posts", (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    });
+});
+
+// Route to get one post
+app.get("/api/getFromId/:id", (req, res) => {
+
+    const id = req.params.id;
+    db.query("SELECT * FROM posts WHERE id = ?", id,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            res.send(result)
+        });
+});
+
+
+//end 
+
+
+
 
 root.render(
     <BrowserRouter basename={baseUrl}>
@@ -25,3 +65,4 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
--- knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/Program.cs
+++ knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
