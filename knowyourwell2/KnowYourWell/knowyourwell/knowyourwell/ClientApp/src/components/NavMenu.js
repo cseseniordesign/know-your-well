cc2ad089ec3590b2deef122132f72f3610ce7b86
William Blazer <william0blazer@gmail.com>
2022-10-31 16:28:41 -0500

    #35 Added placeholder logo to header

--- knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/ClientApp/src/components/NavMenu.js
+++ knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/ClientApp/src/components/NavMenu.js
@@ -25,7 +25,10 @@ export class NavMenu extends Component {
         return (
             <header>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-                    <NavbarBrand tag={Link} to="/">knowyourwell</NavbarBrand>
+                    <NavbarBrand tag={Link} to="/">
+                        <img src={require('./images/placeholder-logo.png')} className="logo"/>
+                    </NavbarBrand>
+                    <NavbarBrand tag={Link} to="/" className="banner">Know Your Well</NavbarBrand>
                     <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
                     <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
                         <ul className="navbar-nav flex-grow">
