4a0631522e5c9f3354a4dbc948c52c21c06609c4
[Joe Davis] <59972343+jdavis511@users.noreply.github.com>
2022-11-01 09:08:39 -0500

    #93 #94 - Hotfix: Deleted Well Log

--- knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/ClientApp/src/components/EditLog.js
+++ knowyourwell2/KnowYourWell/knowyourwell/knowyourwell/ClientApp/src/components/EditLog.js
@@ -28,13 +28,7 @@ export class EditLog extends Component {
             <List style={{ textAlign: 'center' }}>
                 <br />
                 <h2>Edit Well ID </h2>
-                <br />
-                <List.Item >
-                    <List.Content>
-                        <a href="/Prefield" style={{ width: "45%", height: "17%" }} class="btn btn-primary btn-lg btn-block">Well </a>
-                    </List.Content>
-                    <br />
-                </List.Item>
+                <br />            
                 <List.Item>
                     <List.Content>
                         <a href="/Field" style={{ width: "45%", height: "17%" }} class="btn btn-primary btn-lg btn-block">Field</a>
