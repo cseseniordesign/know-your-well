7b17aab3b81b72f9eee5c7b2c0952d383714a07e
William Blazer <william0blazer@gmail.com>
2022-12-08 16:46:55 -0600

    Added webpack config file

---
+++ knowyourwell/knowyourwell/ClientApp/webpack.config.js
@@ -0,0 +1,11 @@
+# webpack.config.js
+
+const path = require("path")
+
+module.exports = {
+    entry: "./src/entry",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "output.js"
+    }
+}
