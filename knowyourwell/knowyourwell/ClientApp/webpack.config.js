fb820859e6b1e9849f98566116d342a9cedff98b
fnaif2021 <89944135+fnaif2021@users.noreply.github.com>
2022-12-12 21:50:55 -0600

    App gets Depoloyed. Setting up the PWA

---
+++ knowyourwell/knowyourwell/ClientApp/webpack.config.js
@@ -0,0 +1,12 @@
+﻿const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
