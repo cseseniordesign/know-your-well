b1251f3cf75f9420e71bda0b099d43dbf48fcb8c
S.Oran <75443930+Orange4337@users.noreply.github.com>
2023-03-07 08:12:55 -0600

    deleted commented button section

--- knowyourwell/knowyourwell/ClientApp/src/components/well.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/well.js
@@ -62,58 +62,5 @@ export default function Well() {
             <h2> <strong> Well Selection </strong></h2>
             {wellList}
         </List>
-        /*
-                            <List style={{ textAlign: 'center' }}>
-                                <h2> <strong> Well Selection </strong></h2>
-                                <List.Item >
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #1 </a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item>
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #2</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item>
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #3</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item>
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #4</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item>
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #5</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item>
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #6</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item>
-                                    <List.Content>
-                                        <a href="/EditWell" style={{ width: "45%", height: "17%" }} className="btn btn-primary btn-lg btn-block">Well #7</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                                <List.Item >
-                                    <List.Content>
-                                        <a href="/WellInfo" style={{ color: 'grey', width: "45%", height: "17%", borderWidth: 3, borderStyle: 'dashed', borderRadius: 1, borderColor: 'grey' }} className="btn btn-light btn-lg btn-block">New Well</a>
-                                    </List.Content>
-                                    <br />
-                                </List.Item>
-                            </List>
-                            */
     );
 }
