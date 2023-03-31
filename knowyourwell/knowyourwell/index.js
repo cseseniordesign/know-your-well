844395781669fa7e118ccd6fa42ff50efb6fde3d
jdavis511 <59972343+jdavis511@users.noreply.github.com>
2023-03-31 18:39:32 -0500

    Update index.js

--- knowyourwell/knowyourwell/index.js
+++ knowyourwell/knowyourwell/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(express.static("/wwwroot"));
+app.use(express.static("wwwroot"));
 
 const config = {
     user: "kywAdmin",
