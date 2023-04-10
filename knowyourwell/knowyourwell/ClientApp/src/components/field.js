03fc8c8d15d8684f7ff9fd88d715eac2a925f69d
fnaif2021 <89944135+fnaif2021@users.noreply.github.com>
2023-04-10 17:52:44 -0500

    delete use-search-params

--- knowyourwell/knowyourwell/ClientApp/src/components/field.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/field.js
@@ -7,7 +7,7 @@ import DatePicker from 'react-datetime';
 import moment from 'moment';
 import 'react-datetime/css/react-datetime.css';
 import useLocalStorage from 'react-use-localstorage';
-import useSearchParams from 'use-search-params'
+import { useSearchParams } from 'react-router-dom'
 
 const conditionsInitilization = () => {
     const Cachedconditions = localStorage.getItem("Conditions");
