4aed98bc8a6390f53147e202ff70f4484e905867
[Joe Davis] <59972343+jdavis511@users.noreply.github.com>
2023-04-11 10:52:52 -0500

    Adjusted credentials

--- knowyourwell/knowyourwell/index.js
+++ knowyourwell/knowyourwell/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static("wwwroot"));
 
 const config = {
-    user: "kywAdmi",
+    user: "kywAdmin",
     password: "KJ6vcCG2",
     database: "kyw",
     server: 'kyw.database.windows.net',
