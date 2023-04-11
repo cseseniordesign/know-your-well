1b5ff1c524055bfbeeeaa85886b56a740c974ae5
[Joe Davis] <59972343+jdavis511@users.noreply.github.com>
2023-04-11 12:33:30 -0500

    #179 - querying for previous entries and processing data.

--- knowyourwell/knowyourwell/ClientApp/src/components/previousentries.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/previousentries.js
@@ -1,10 +1,74 @@
-﻿import React from 'react'
+﻿import React, { useEffect, useState } from 'react';
 import { List } from 'semantic-ui-react'
+import { useSearchParams } from "react-router-dom";
+import Axios from 'axios'
+import FieldSelection from './fieldselection';
+
+var previousEntries = []
+var listElements = []
+
+function generatelistElements(previousEntries) {
+    for (const entry in PreviousEntries) {
+        let key = 0
+        listElements.push(
+            <>
+                <List.Item key={key}>
+                    <List.Content>
+                        <a href={'/ViewField?fieldactivity_id=${entry[0]}'} style={{ width: "22.5%", height: "17%" }} class="btn btn-primary btn-lg">Field</a>
+                        <a href={'/ViewClassLab?classlab_id=${entry[1]}'} style={{ width: "22.5%", height: "17%" }} class="btn btn-primary btn-lg">Class Lab</a>
+                    </List.Content>
+                    <br />
+                </List.Item>
+            </>
+        );
+        key++
+    }
+    return listElements
+}
 
 export default function PreviousEntries() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const well_id = parseInt(searchParams.get("id"));
+    const wellName = searchParams.get("wellName")
 
     const backbutton = () => {
-        window.location.href = "/editwell";
+        window.location.href = `/EditWell?id=${well_id}&wellName=${wellName}`;
+    }
+
+    const [isLoading, setLoading] = useState(true);
+
+    //credit to https://codewithnico.com/react-wait-axios-to-render/ for conditional rendering
+    useEffect(() => {
+        Axios
+            .get("/FieldList", {
+                responseType: "json",
+                params: {
+                    well_id: well_id
+                }
+            })
+            .then(function (response) {
+                console.log(response)
+                const fieldList = response.data.FieldList;
+                for (const fieldEntry of fieldList) {
+                    Axios
+                        .get("/LabID", {
+                            responseType: "json",
+                            params: {
+                                fieldactivity_id: parseInt(fieldEntry.fieldactivity_id)
+                            }
+                        })
+                        .then(function (response) {
+                            previousEntries.push(parseInt(fieldEntry.fieldactivity_id), parseInt(response.data.LabID))
+                        });
+                }
+                listElements = generatelistElements(previousEntries)
+                console.log(listElements.length)
+                setLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <h1>Loading</h1>
     }
 
     return (
@@ -16,24 +80,7 @@ export default function PreviousEntries() {
             <List.Item>
                 <h4>[Field Date]</h4>
             </List.Item>
-            <List.Item>
-                <List.Content>
-                    <a href="/ViewField" style={{ width: "22.5%", height: "17%" }} class="btn btn-primary btn-lg">Field</a>
-                    <a href="/ViewClassLab" style={{ width: "22.5%", height: "17%" }} class="btn btn-primary btn-lg">Class Lab</a>
-                </List.Content>
-                <br />
-            </List.Item>
-
-            <List.Item>
-                <h4>[Field Date]</h4>
-            </List.Item>
-            <List.Item>
-                <List.Content>
-                    <a href="/ViewField" style={{ width: "22.5%", height: "17%" }} class="btn btn-primary btn-lg">Field</a>
-                    <a href="/ViewClassLab" style={{ width: "22.5%", height: "17%" }} class="btn btn-primary btn-lg disabled" aria-disabled="true">Class Lab</a>
-                </List.Content>
-                <br />
-            </List.Item>
+            {listElements}
 
             <List.Item>
                 <List.Content>
