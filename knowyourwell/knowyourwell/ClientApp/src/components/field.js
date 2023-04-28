9cebb87b378536b68504873a400a5ed4f45ac1f8
AbigailWunderlich <abbybrooks8@gmail.com>
2023-04-28 13:50:55 -0500

    added functionality for general lat/longitude calculations

--- knowyourwell/knowyourwell/ClientApp/src/components/field.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/field.js
@@ -32,8 +32,8 @@ export default function Field() {
     const wellName = searchParams.get("wellName");
     const [fa_latitude, setFa_latitude] = useState(pullCachedData ? cachedData.fa_latitude : "");
     const [fa_longitude, setFa_longitude] = useState(pullCachedData ? cachedData.fa_longitude : "");
-    const fa_genlatitude = 40.8;   //TODO: match this up with actual value.
-    const fa_genlongitude = -97.5; //TODO: match this up with actual value.
+    const fa_genlatitude = Math.round(fa_latitude * 100) / 100;
+    const fa_genlongitude = Math.round(fa_latitude * 100) / 100;
     
     const [conditions, setConditions] = useState(pullCachedData ? cachedData.Conditions : "");
     const [pooling, setPooling] = useState(pullCachedData ? cachedData.Pooling : "");
