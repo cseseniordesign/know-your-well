bf6fcec6cd5cfc656e4984809e173a518e928b60
Tanner Turgeon <tturgeon4@huskers.unl.edu>
2023-10-12 14:58:14 -0500

    Updated where the "* = Required Field" was located

--- knowyourwell/knowyourwell/ClientApp/src/components/wellinfo.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/wellinfo.js
@@ -191,6 +191,10 @@ export default function WellInfo() {
     return (
         <form action="/editwell" id="submissionAlert" >
             <h2>Well Info</h2>
+            <div className="requiredField">
+                <br/>
+                * = Required Field
+            </div>
             <div className="css">
                 <label for="wellname">
                     Well Name:
@@ -780,10 +784,7 @@ export default function WellInfo() {
             <br/>
             <button type="button" style={{ width: "180px", height: "17%" }} className="btn btn-primary btn-lg" onClick={submitForm}>Submit</button>
             <button type="button" style={{ width: "180px", height: "17%" }} className="btn btn-primary btn-lg" onClick={backButton}>Back</button>
-            <div className="requiredField">
-                <br/>
-                * = Required Field
-            </div>
+            
         </form>
     );
 }
