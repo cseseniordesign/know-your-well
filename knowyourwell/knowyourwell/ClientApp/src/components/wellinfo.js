1e395a317aa59ccb74f893d121a32cdafe684ea7
Tanner Turgeon <tturgeon4@huskers.unl.edu>
2023-10-19 16:25:59 -0500

    Added "* = Required Field and added validation on Water Level Depth and Total Well Depth

--- knowyourwell/knowyourwell/ClientApp/src/components/wellinfo.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/wellinfo.js
@@ -28,12 +28,11 @@ export default function WellInfo() {
     const [estlatitude, setEstlatitude] = useState(null);
     const [estlongitude, setEstlongitude] = useState(null);
     const [boreholediameter, setBoreholediameter] = useState(0);
-    const [totaldepth, setTotaldepth] = useState(0);
-    const [well_waterleveldepth, setWell_waterleveldepth] = useState(0);
     const [wellcasematerial, setWellcasematerial] = useState("");
     const [datacollector, setDatacollector] = useState("");
     const [observation, setObservation] = useState("");
     const [dateentered, setDateentered] = useState(moment().format('L, h:mm a'));
+    const [error, setError] = useState(0);
 
     const date = new Date();
     const futureDate = date.getDate();
@@ -102,7 +101,28 @@ export default function WellInfo() {
     const [welltype, setWelltype] = useState("");
     const handleChange_welltype = (event) => {
         setWelltype(event.target.value);
-    };
+    };    
+    const [well_waterleveldepth, setWell_waterleveldepth] = useState("");
+    const handleChange_waterleveldepth = (event) => {
+        if(event.target.value > totaldepth) {
+            setError(1);
+            setWell_waterleveldepth("");
+        } else {
+            setError(0);
+            setWell_waterleveldepth(event.target.value);
+        }
+    }
+
+    const [totaldepth, setTotaldepth] = useState("");
+    const handleChange_totaldepth = (event) => {
+        if(event.target.value < well_waterleveldepth) {
+            setError(1);
+            setTotaldepth("");
+        } else {
+            setError(0);
+            setTotaldepth(event.target.value);
+        }
+    }
 
     function addWellInfo() {
         Axios.post('/createwellinfo', {
@@ -647,10 +667,8 @@ export default function WellInfo() {
                 <label for="totaldepth">
                     Total depth of well (feet):
                 </label>
-                <input type="text" className="textarea resize-ta" id="totaldepth" name="totaldepth" pattern="[0-9]+([.][0-9]{1,5})?"
-                    onChange={(event) => {
-                        setTotaldepth(event.target.value);
-                    }}
+                <input type="text" className="textarea resize-ta" id="totaldepth" name="totaldepth" pattern="[0-9]+([.][0-9]{1,5})?" value={totaldepth}
+                    onChange={(handleChange_totaldepth)}
                 />
             </div>
             <div className="css">
@@ -674,10 +692,11 @@ export default function WellInfo() {
                 <label for="well_waterleveldepth">
                     Water level (feet):
                 </label>
-                <input type="text" className="textarea resize-ta" id="well_waterleveldepth" name="well_waterleveldepth" pattern="[0-9]+([.][0-9]{1,5})?"
-                    onChange={(event) => {
-                        setWell_waterleveldepth(event.target.value);
-                    }}
+                <div className="waterMoreThanTotal">
+                    Water level depth cannot be greater than total well depth
+                </div>
+                <input type="text" className="textarea resize-ta" id="well_waterleveldepth" name="well_waterleveldepth" pattern="[0-9]+([.][0-9]{1,5})?" value={well_waterleveldepth}
+                    onChange={(handleChange_waterleveldepth)}
                 />
             </div>
             <div className="css">
@@ -782,9 +801,10 @@ export default function WellInfo() {
                 </div>
             </div>
             <br/>
-            <button type="button" style={{ width: "180px", height: "17%" }} className="btn btn-primary btn-lg" onClick={submitForm}>Submit</button>
+            <button type="button" style={{ width: "180px", height: "17%" }} className="btn btn-primary btn-lg" 
+                onClick={error ? window.alert("Water level depth CANNOT be greater than total well depth.") : submitForm}>Submit
+            </button>
             <button type="button" style={{ width: "180px", height: "17%" }} className="btn btn-primary btn-lg" onClick={backButton}>Back</button>
-            
         </form>
     );
 }
