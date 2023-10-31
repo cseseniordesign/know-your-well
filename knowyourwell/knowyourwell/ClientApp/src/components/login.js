be493a3a855bd5888222c5e7e00f9e4c0c27d720
bkardell13 <143013570+bkardell13@users.noreply.github.com>
2023-10-31 17:41:27 -0500

    Improved login screen

--- knowyourwell/knowyourwell/ClientApp/src/components/login.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/login.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './css/login_signup.css';
-import Axios from 'axios'
 
 export default function Login() {
 
-    const initRedirectRequest = () => {
-        Axios
-            .get("/sso/redirect")
-            .then(function (response) {
-                console.log("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-                console.log(response);
-            });
-    };
-
     return (
-        <div className="d-grid" >
-            <a href="/Well" className="btn btn-primary" >Log in to Nebraska Cloud</a>
-            <p></p>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            
+            <div style={{ flex: 1 }}></div>
+            
+            <div style={{ flex: 1 }}>
+                <h3 style={{ textAlign: "center", paddingBottom: "1em" }}>
+                    Welcome to Know Your Well
+                </h3>
+                <div className="d-grid">
+                    <a href="/Well" className="btn btn-primary">
+                        Login Using Nebraska Cloud
+                    </a>
+                </div>
+            </div>
+            
+            <div style={{ flex: 1 }}></div>
+            
         </div>
     )
 }
-// onClick={initRedirectRequest}
-// href="/Well"
