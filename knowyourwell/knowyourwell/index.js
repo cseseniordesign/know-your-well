647d1ef71d784c2596705d041ba8d5d1278bd335
bkardell13 <143013570+bkardell13@users.noreply.github.com>
2024-02-15 16:28:55 -0600

    actually fixed bug

--- knowyourwell/knowyourwell/index.js
+++ knowyourwell/knowyourwell/index.js
@@ -36,6 +36,7 @@ try {
     config = JSON.parse(rawData);
 } catch (e) {
     config = {
+        user: "kywAdmin",
         password: process.env.APPSETTING_MSSQL_PASSWORD,
         database: "kyw",
         server: 'kyw.database.windows.net',
