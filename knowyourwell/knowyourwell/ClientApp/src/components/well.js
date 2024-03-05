16f7bed17c28324a98ddc666325cdb1aa563b455
bkardell13 <143013570+bkardell13@users.noreply.github.com>
2024-03-05 15:31:36 -0600

    conditional rendering

--- knowyourwell/knowyourwell/ClientApp/src/components/well.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/well.js
@@ -102,70 +102,73 @@ useEffect(() => {
 //         </List>
 //     );
 // }
-if (isLoading) {
-    return <h1>Loading</h1>
-}
+
 
 const handleBlur = (event) => {
     if (containerRef.current && !containerRef.current.contains(event.relatedTarget)) {
         setFilterDropdownVisibility(false);
     }
 };
-
-return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-        <div style={{ flex: 30, textAlign: 'center' }}>
-            <div ref={containerRef}>
-                <button onClick={() => { setSortDropdownVisibility(!isSortDropdownVisible); }} className="btn btn-primary">Sort Wells</button>
-                {isSortDropdownVisible && (
-                    <div style={{
-                        border: '1px solid #ccc',
-                        marginTop: '10px',
-                        boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
-                        boxSizing: 'border-box'
-                    }}>
-                        <button onClick={() => setSort("undefined")} style={{ backgroundColor: sort === "undefined" ? 'yellow' : 'transparent' }} className="dropdown-item">Clear Sort</button>
-                        <button onClick={() => setSort("well_id")} style={{ backgroundColor: sort === "well_id" ? 'yellow' : 'transparent' }} className="dropdown-item">Oldest-Newest</button>
-                        <button onClick={() => setSort("well_id DESC")} style={{ backgroundColor: sort === "well_id DESC" ? 'yellow' : 'transparent' }} className="dropdown-item">Newest-Oldest</button>
-                    </div>
-                )}
-                <button onClick={() => { setFilterDropdownVisibility(!isFilterDropdownVisible); }} className="btn btn-primary">Filter By County</button>
-                {isFilterDropdownVisible && (
-                    <div
-                        style={{
+if (isLoading) {
+    return <h1>Loading</h1>
+}
+else {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+    
+            <div style={{ flex: 30, textAlign: 'center' }}>
+                <div ref={containerRef}>
+                    <button onClick={() => { setSortDropdownVisibility(!isSortDropdownVisible); }} className="btn btn-primary">Sort Wells</button>
+                    {isSortDropdownVisible && (
+                        <div style={{
                             border: '1px solid #ccc',
                             marginTop: '10px',
                             boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
-                            boxSizing: 'border-box',
-                            maxHeight: '150px',
-                            overflow: 'auto'
+                            boxSizing: 'border-box'
                         }}>
-                        <button onClick={() => setFilter("undefined")} style={{ backgroundColor: filter === "undefined" ? 'yellow' : 'transparent' }} className="dropdown-item">Clear Filter</button>
-                        {countyOptions.map(county => (
-                            <button key={county.key}
-                                onClick={() => setFilter(`county_id = '${county.key}'`)}
-                                style={{ backgroundColor: filter === `county_id = '${county.key}'` ? 'yellow' : 'transparent' }}
-                                className="dropdown-item">
-                                {county.value}
-                            </button>
-                        ))}
-                    </div>
-                )}
+                            <button onClick={() => setSort("undefined")} style={{ backgroundColor: sort === "undefined" ? 'yellow' : 'transparent' }} className="dropdown-item">Clear Sort</button>
+                            <button onClick={() => setSort("well_id")} style={{ backgroundColor: sort === "well_id" ? 'yellow' : 'transparent' }} className="dropdown-item">Oldest-Newest</button>
+                            <button onClick={() => setSort("well_id DESC")} style={{ backgroundColor: sort === "well_id DESC" ? 'yellow' : 'transparent' }} className="dropdown-item">Newest-Oldest</button>
+                        </div>
+                    )}
+                    <button onClick={() => { setFilterDropdownVisibility(!isFilterDropdownVisible); }} className="btn btn-primary">Filter By County</button>
+                    {isFilterDropdownVisible && (
+                        <div
+                            style={{
+                                border: '1px solid #ccc',
+                                marginTop: '10px',
+                                boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+                                boxSizing: 'border-box',
+                                maxHeight: '150px',
+                                overflow: 'auto'
+                            }}>
+                            <button onClick={() => setFilter("undefined")} style={{ backgroundColor: filter === "undefined" ? 'yellow' : 'transparent' }} className="dropdown-item">Clear Filter</button>
+                            {countyOptions.map(county => (
+                                <button key={county.key}
+                                    onClick={() => setFilter(`county_id = '${county.key}'`)}
+                                    style={{ backgroundColor: filter === `county_id = '${county.key}'` ? 'yellow' : 'transparent' }}
+                                    className="dropdown-item">
+                                    {county.value}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                </div>
+    
+                <List>
+                    <h2><strong>Wells</strong></h2>
+                    <List.Item key={-1}>
+                        <List.Content>
+                            <a href={`/WellInfo`} style={{ width: "45%", height: "17%", border: "dashed" }} className="btn btn-light btn-lg btn-block">Create New Well</a>
+                        </List.Content>
+                        <br />
+                    </List.Item>
+                    {wellList}
+                </List>
             </div>
-
-            <List>
-                <h2><strong>Wells</strong></h2>
-                <List.Item key={-1}>
-                    <List.Content>
-                        <a href={`/WellInfo`} style={{ width: "45%", height: "17%", border: "dashed" }} className="btn btn-light btn-lg btn-block">Create New Well</a>
-                    </List.Content>
-                    <br />
-                </List.Item>
-                {wellList}
-            </List>
+    
         </div>
+    );
+}
 
-    </div>
-);
 }
