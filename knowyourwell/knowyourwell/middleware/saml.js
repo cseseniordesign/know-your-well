1e57cdcc507eaa628f1bce333e7bdae4a284b367
bkardell13 <143013570+bkardell13@users.noreply.github.com>
2024-04-30 16:58:24 -0500

    potential ne cloud fix

--- knowyourwell/knowyourwell/middleware/saml.js
+++ knowyourwell/knowyourwell/middleware/saml.js
@@ -13,27 +13,28 @@ const idp = samlify.IdentityProvider({
 
 
 let hitemplate = `<samlp:AuthnRequest 
-    xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"     
-    xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" 
-    ID="{ID}" 
-    Version="2.0" 
-    IssueInstant="{IssueInstant}" 
-    Destination="{Destination}" 
-    ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" 
-    AssertionConsumerServiceURL="{AssertionConsumerServiceURL}">
+xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"     
+xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" 
+ID="{ID}" 
+Version="2.0" 
+IssueInstant="{IssueInstant}" 
+Destination="https://idp.nebraskacloud.org/simplesaml/saml2/idp/SSOService.php" 
+ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" 
+AssertionConsumerServiceURL="https://knowyourwell.azurewebsites.net/saml/acs">
 
-    <saml:Issuer>{Issuer}</saml:Issuer>
-    <samlp:NameIDPolicy 
-        Format="{NameIDFormat}" 
-        AllowCreate="false"/>
-        
-    </samlp:AuthnRequest>`;
+<saml:Issuer>https://knowyourwell.azurewebsites.net/</saml:Issuer>
+<samlp:NameIDPolicy 
+    Format="urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"
+    AllowCreate="false"/>
+    
+</samlp:AuthnRequest>`;
+
     const path = require('path');
     const filePath = path.join(__dirname, 'customLoginRequest.xml');
 // Configure service provider from this app's metadata
 const sp = samlify.ServiceProvider({
     loginRequestTemplate: {
-        context: fs.readFileSync(filePath, 'utf8'),
+        context: hitemplate,
       },
     metadata: fs.readFileSync(__dirname + '/../metadata/sp.xml'),
 });
