dd986ec2f19aac857ac0ec21bbf5d974e02d95e5
Jacob Amisano <jamisano2@huskers.unl.edu>
2024-05-07 14:34:09 -0500

    Photo Upload feature for well heads

--- knowyourwell/knowyourwell/ClientApp/src/components/reusable/photoUpload.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/reusable/photoUpload.js
@@ -1,37 +1,25 @@
 // const { DefaultAzureCredential } = require("@azure/identity");
 const { BlobServiceClient } = require("@azure/storage-blob");
 
-export default async function uploadPhotos(file) {
-  const AZURE_STORAGE_CONNECTION_STRING = "BlobEndpoint=https://knowyourwell.blob.core.windows.net/;QueueEndpoint=https://knowyourwell.queue.core.windows.net/;FileEndpoint=https://knowyourwell.file.core.windows.net/;TableEndpoint=https://knowyourwell.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-04-30T16:31:47Z&st=2024-03-29T08:31:47Z&spr=https&sig=p0n52uL62ApmdV2Fry%2FT9q8xXemuZApIGGO9Ig6Pbdk%3D";
-
-  if (!AZURE_STORAGE_CONNECTION_STRING) {
-      throw Error('Azure Storage Connection string not found');
-  }
+export default async function uploadPhoto(file, container, name, fileName) {
   try{
-    console.log("Azure Blob storage v12 - JavaScript quickstart sample");
     const AZURE_STORAGE_CONNECTION_STRING = "BlobEndpoint=https://knowyourwell.blob.core.windows.net/;QueueEndpoint=https://knowyourwell.queue.core.windows.net/;FileEndpoint=https://knowyourwell.file.core.windows.net/;TableEndpoint=https://knowyourwell.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-05-30T16:04:45Z&st=2024-03-29T08:04:45Z&spr=https&sig=DzJDElMlVzXS4Cz2ALsz6FwKjNB2gCkOr44MIU0a35w%3D";
     if (!AZURE_STORAGE_CONNECTION_STRING) {
       throw Error('Azure Storage Connection string not found');
     }
     const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-
-    const containerName = 'photo-upload-test';
+    const containerName = "wellid-"+container;
     const containerClient = blobServiceClient.getContainerClient(containerName);
-    const blobName = file.name; //Change This 
+
+    try{
+      await containerClient.createIfNotExists();
+    } catch (err) {
+
+    }
+    const blobName = name;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    const data = 'Hello World!'; //Change This
-    alert('Before Blob Response');
-    // debugger;
-    const uploadBlobResponse = await blockBlobClient.uploadBrowserData(file).then((value) => {
-      alert(value); // 'Success!'
-      debugger;
-    }).catch((error) => {
-      alert('Error: ' + error);
-    });
-
-    alert(uploadBlobResponse);
-    alert(`Blob was uploaded successfully. requestId: ${uploadBlobResponse}`);
+    await blockBlobClient.uploadData(file)
   } catch (err) {
     alert(`Error: ${err.message}`);
     throw err;
