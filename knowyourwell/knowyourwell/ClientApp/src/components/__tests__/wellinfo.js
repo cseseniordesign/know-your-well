8ff74578c0f635c7812cb174a77b7e799f98e860
Katelyn Breitbarth <kbreitbarth2@huskers.unl.edu>
2024-10-12 11:52:33 -0500

    Test update

--- knowyourwell/knowyourwell/ClientApp/src/components/__tests__/wellinfo.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/__tests__/wellinfo.js
@@ -8,17 +8,17 @@ import axios from "axios";
 import WellInfo, { generateWellcode } from "../wellinfo";
 
 
-// jest.mock('axios');
-// describe("the function that generates the well code", () => {
-//   it('returns the proper well code', async () => {
-//     axios.get.mockResolvedValue({
-//     data:
-//         {
-//             wellcode: "UNL038",
-//         },
-//     });
+jest.mock('axios');
+describe("the function that generates the well code", () => {
+  it('returns the proper well code', async () => {
+    axios.get.mockResolvedValue({
+    data:
+        {
+            wellcode: "UNL001",
+        },
+    });
 
-//     const testCode = await generateWellcode();
-//     expect(testCode).toEqual("UNL038");
-//   });
-// });
+    const testCode = await generateWellcode();
+    expect(testCode).toEqual("UNL001");
+  });
+});
