b22783ec8be65aa5bb948a6f17ce98743301cc60
Katelyn Breitbarth <kbreitbarth2@huskers.unl.edu>
2025-01-29 14:40:56 -0600

    Anchored the icon properly

--- knowyourwell/knowyourwell/ClientApp/src/components/well.js
+++ knowyourwell/knowyourwell/ClientApp/src/components/well.js
@@ -42,8 +42,8 @@ function responseDataToMarkerList(responseData) {
   try {
     for (const element of responseData) {
       markerList.push(
-        <Marker key={element.wi_wellcode} position={[element.wi_estlatitude, element.wi_estlongitude]} icon={new Icon({iconUrl: markerIconPng, iconSize: [30, 41]})}>
-          <Popup>
+        <Marker key={element.wi_wellcode} position={[element.wi_estlatitude, element.wi_estlongitude]} icon={new Icon({iconUrl: markerIconPng, iconSize: [30, 30], iconAnchor: [15, 30]})}>
+          <Popup position={[0, 0]}>
             <a href={`/EditWell?id=${element.well_id}&wellName=${element.wi_wellname}&wellcode=${element.wi_wellcode}`}> {element.wi_wellcode}: {element.wi_wellname}</a><br />
             Owned by: {element.wi_well_owner}
 
